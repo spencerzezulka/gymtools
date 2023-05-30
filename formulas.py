60ee8e034225b6379e8599d7d07d150a8ec59164
Spencer <spencer_zezulka@berkeley.edu>
2023-05-30 14:02:13 -0700

    removed astropy dependency

--- formulas.py
+++ formulas.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import astropy.units as u
-import astropy.constants as c
+# import astropy.units as u
+# import astropy.constants as c
 import altair as alt
 
 class GymBro():
